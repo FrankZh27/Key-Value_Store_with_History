50b0bf09bfb9ba9e2d503f8712858f8fba0af1ab
FrankZh27 <jzho@umich.edu>
2018-09-19 18:59:50 -0700

    update comments

--- store_with_history_v1.py
+++ store_with_history_v1.py
@@ -11,40 +11,58 @@ import pprint
 import datetime
 
 def convert_string_to_time(date_string):
+# convert string to datatime type, so that we can
+# compare two times directly later
     from datetime import datetime
     date_time_obj = datetime.strptime(date_string[:26], '%Y-%m-%d %H:%M:%S.%f')
     return date_time_obj
 
 def get(db, key):
+# Give a list of friends a specific key has.
+# When consider if a value in the result list,
+# we compare the last insert time and the last
+# remove time
     obj = db.user.find_one({"name":key})
     if obj == None:
         print('Sorry, we do not have this name in our database.')
         return
-    friends_obj = obj[friends]
+    friends_obj = obj['friends']
     friends_list = []
-    for friend in friend_obj:
-        time_list = friend_obj[friend]
+    for friend in friends_obj:
+        time_list = friends_obj[friend]
         if len(time_list[1]['remove']) == 0:
-            friend_list.append(friend)
+            friends_list.append(friend)
         else:
             add_time = convert_string_to_time(time_list[0]['add'][-1])
             remove_time = convert_string_to_time(time_list[1]['remove'][-1])
             if add_time > remove_time:
-                friend_list.append(friend)
+                friends_list.append(friend)
     return friends_list
 
 def put(db, key, value):
+# insert a key-value pair in the db. First we need to consider
+# if the key exists, then we consider if the value already exists.
+# Finally, we consider if the value has been removed before or not.
     obj = db.user.find_one({"name":key})
     time = str(datetime.datetime.now())
     if obj == None:
         db.user.insert_one({"name":key, "friends":{value:[{"add":[time]}, {"remove":[]}]}})
     elif value not in obj['friends']:
         obj['friends'].add({value:[{"add":[time]},{"remove":[]}]})
+    elif len(obj['friends'][value][1]['remove']) == 0:
+        return
     else:
-        obj['friends'][value][0]['add'].append(time)
+        last_add = convert_string_to_time(obj['friends'][value][0]['add'][-1])
+        last_remove = convert_string_to_time(obj['friends'][value][1]['remove'][-1])
+        if last_add <= last_remove:
+            obj['friends'][value][0]['add'].append(time)
     return
     
 def delete(db, key):
+# Delete the information according to a specific key.
+# Here once we delete the key(a user), we destroy everything
+# it has. It make sense because if we think of a user deactivating its
+# account, all of its information should be removed.
     obj = db.user.find_one({"name":key})
     if obj == None:
         print('Sorry, key is not found.')
@@ -53,6 +71,9 @@ def delete(db, key):
     return
     
 def delete(db, key, value):
+# Delete the value according to a specific key. First we need to
+# consider if the key is already exists, then we consider if the
+# value exists.
     obj = db.user.find_one({"name":key})
     time = str(datetime.datetime.now())
     if obj == None:
@@ -61,20 +82,60 @@ def delete(db, key, value):
         print('Sorry, value is not found.')
     else:
         obj['friends'][value][1]['remove'].append(time)
-    return 
-    
-def get(key, time):
+    return
+
+def is_there(time, time_list):
+# Use this API to consider if a specific value(friend) is in the
+# list at current time.
+    time_flag = convert_string_to_time(time)
+    add_time = time_list[0]['add']
+    remove_time = time_list[1]['remove']
+    if len(remove_time) == 0:
+        return True
+    if time_flag < convert_string_to_time(add_time[0]):
+        return False
+    if time_flag < convert_string_to_time(remove_time[0]):
+        return True
+    last_add = add_time[0]
+    last_remove = remove_time[0]
+    for i in range(add_time):
+        if convert_string_to_time(add_time[i]) > time_flag:
+            last_add = convert_string_to_time(add_time[i-1])
+    for i in range(remove_time):
+        if convert_string_to_time(remove_time[i]) > time_flag:
+            last_remove = convert_string_to_time(remove_time[i-1])
+    if last_add <= last_remove:
+        return False
+    else:
+        return True
+
+def get(db, key, time):
+# Get the friend list according to a speific key
     obj = db.user.find_one({"name":key})
     friends_list = []
     if obj == None:
         print('Sorry, key is not found.')
     else:
         for friend in obj['friends']:
-            if is_there(time, friend):
+            if is_there(time, obj['friends'][friend]):
                 friends_list.append(friend)
     return friends_list
     
-def diff(key, time1, time2):
+def diff(db, key, time1, time2):
+    list1 = get(db, key, time1)
+    list2 = get(db, key, time2)
+    set1 = set(list1)
+    set2 = set(list2)
+    ans = []
+    for item in set1:
+        if item not in set2:
+            ans.append(item)
+    for item in set2:
+        if item not in set1:
+            ans.append(item)
+    return ans
+    
+    
     
 
 def main():
